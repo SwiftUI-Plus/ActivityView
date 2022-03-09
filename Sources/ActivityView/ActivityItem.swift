05a4b80d4977263df7d355b4e0bde7a07483a66e
Jerry Keough <delawaremathguy@icloud.com>
2022-03-09 11:05:49 -0500

    Simplified variadic intializer.

--- Sources/ActivityView/ActivityItem.swift
+++ Sources/ActivityView/ActivityItem.swift
@@ -1,22 +1,20 @@
 import UIKit
 
-/// Represents an activity for presenting an ActivityView (Share sheet) via the `activitySheet` modifier
+	/// Represents an activity for presenting an ActivityView (Share sheet) via the `activitySheet` modifier
 public struct ActivityItem {
-
-    internal var items: [Any]
-    internal var activities: [UIActivity]
-    internal var excludedTypes: [UIActivity.ActivityType]
-
-    /// The
-    /// - Parameters:
-    ///   - items: The items to share via a `UIActivityViewController`
-    ///   - activities: Custom activities you want to include in the sheet
-    public init(items: Any..., activities: [UIActivity] = [], excludedTypes: [UIActivity.ActivityType] = []) {
-        self.items = items
-        self.activities = activities
-        self.excludedTypes = excludedTypes
-    }
-    
+	
+	internal var items: [Any]
+	internal var activities: [UIActivity]
+	internal var excludedTypes: [UIActivity.ActivityType]
+	
+		/// The variadic form of the initializer
+		/// - Parameters:
+		///   - items: The items to share via a `UIActivityViewController`
+		///   - activities: Custom activities you want to include in the sheet
+	public init(items: Any..., activities: [UIActivity] = [], excludedTypes: [UIActivity.ActivityType] = []) {
+		self.init(items: items, activities: activities, excludedTypes: excludedTypes)
+	}
+	
 		/// An alternative initializer using array syntax (not variadic syntax)
 		/// - Parameters:
 		///   - items: The array of items to share via a `UIActivityViewController`
@@ -26,5 +24,5 @@ public struct ActivityItem {
 		self.activities = activities
 		self.excludedTypes = excludedTypes
 	}
-
+	
 }
